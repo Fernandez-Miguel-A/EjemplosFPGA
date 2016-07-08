db969a8f19e301b3d956e8efd7987e807babb830
alejandrogroso@hotmail.com <Salmista-94>
2016-07-07 21:03:36 -0300

    Controlador Top de Sistema (DEFINITIVO)!!!

--- Trabajo_Final/top_Controlador.vhd
+++ Trabajo_Final/top_Controlador.vhd
@@ -27,59 +27,44 @@ use IEEE.STD_LOGIC_UNSIGNED.ALL;
 --library UNISIM;
 --use UNISIM.VComponents.all;
 
-entity uart_eje1 is
+entity Sistema is
     Port ( clk, rst : in  STD_LOGIC;
            entrada_serial : in  STD_LOGIC;
 			  switchs : in  STD_LOGIC_VECTOR (3 downto 0);
            pulsador : in  STD_LOGIC;
            leds : out STD_LOGIC_VECTOR (7 downto 0));
-end uart_eje1;
+end Sistema;
 
 
 
-architecture Behavioral of uart_eje1 is
+architecture Behavioral of Sistema is
 signal interrupcion_pulsador: std_logic;
-signal from_PC : STD_LOGIC;
+signal data_SWITCH : STD_LOGIC_VECTOR (7 downto 0);
+signal data_UART : STD_LOGIC_VECTOR (7 downto 0);
 
 signal data_IN : STD_LOGIC_VECTOR (7 downto 0);
---signal data_leds : STD_LOGIC_VECTOR (7 downto 0);
+signal streaming_BYTE : STD_LOGIC_VECTOR (7 downto 0);
 
 
---SEÑALES para los leds
 signal pre_leds : STD_LOGIC_VECTOR (7 downto 0);
 
---SEÑALES para registro pre leds
---signal entrada_seg : STD_LOGIC_VECTOR (3 downto 0);
-signal datos_entrada : STD_LOGIC_VECTOR (7 downto 0);
-signal datos_salida : STD_LOGIC_VECTOR (7 downto 0);
-
 --SEÑALES para UART
-signal UART_serial_in : STD_LOGIC;
-signal UART_data_out : STD_LOGIC_vector(7 downto 0);
 signal UART_read_buffer : STD_LOGIC;
 signal UART_buffer_data_present : STD_LOGIC;
 signal UART_buffer_full : STD_LOGIC;
 signal UART_buffer_half_full : STD_LOGIC;
 
 --SEÑALES para PICO_BLAZE
-signal direcc : std_logic_vector(9 downto 0);
-signal instruction : std_logic_vector(17 downto 0);
 signal writing : std_logic;
-signal reading : std_logic;
-signal streaming_byte : std_logic_vector(7 downto 0);
 signal interrupted : std_logic;
 
 signal interrupt_ack : std_logic;
-signal interrupt_ack1 : std_logic;
+signal interrupt_UART : std_logic;
+signal interrupt_PULSADOR : std_logic;
 
-signal interrupt_ack2 : std_logic;
-signal port_id_PICO : std_logic_vector(7 downto 0);
 
---SEÑALES para registro1
---signal habilitar : STD_LOGIC; -- reemplazado por 'interrupt_ack'
-
---SEÑALES para registro2
-signal habilitar : STD_LOGIC;
+--type fuente_interrupt is (from_PC,from_PULSADOR);
+--signal FUENTE_I: fuente_interrupt;
 
 
 component UART_4800 is
@@ -127,14 +112,14 @@ end component;
 begin
 
 RX_UART: UART_4800 
-port map (clk => clk, rst => rst, next_data => interrupt_ack1,
-	entrada_serial => entrada_serial, dato_sal => streaming_byte,
+port map (clk => clk, rst => rst, next_data => interrupt_UART,
+	entrada_serial => entrada_serial, dato_sal => streaming_BYTE,
 	leer_data => UART_read_buffer, data_present => UART_buffer_data_present,
    buff_full => UART_buffer_full, buff_half => UART_buffer_half_full);
 
 microcontrolador: top_PB 
 port map (write_strobe => writing, out_port => pre_leds, read_strobe => UART_read_buffer,
-    in_port => data_IN, interrupt => interrupted, port_id => port_id_PICO,
+    in_port => data_IN, interrupt => interrupted,
     interrupt_ack => interrupt_ack, reset => rst, clk => clk);
 
     
@@ -142,33 +127,33 @@ registro2: registro_datos
 port map (clk => clk, rst => rst, in_data=> pre_leds,
     out_data=> leds, habilitador => writing);
 
-  
---necesito una maquinita de estados para determinar cual
---es la se�al FUENTE, la UART � el 'boton estabilizado';
---si es el boton debo Normalizar!.
-
 
 contr_boton: controlador_boton 
-port map (clk => clk, rst => rst, ack => interrupt_ack2,
+port map (clk => clk, rst => rst, ack => interrupt_PULSADOR,
 	pulsador=> pulsador, norm_signal => interrupcion_pulsador);
 
 
 
+data_SWITCH <= "0000"&switchs;
+data_UART <= '1'&streaming_BYTE(6 downto 0);
+
+
 interrupted <= interrupcion_pulsador or UART_buffer_data_present;
-data_IN <= streaming_byte when from_PC = '1' else ("0000"&switchs);
 
-from_PC <= '1' when interrupcion_pulsador = '1' and UART_buffer_data_present = '1' else
-			  '0' when interrupcion_pulsador = '1' else '1';
-			  
-			  
---interrupt_ack1 <= interrupt_ack; -- en realidad debo 
---interrupt_ack2 <= interrupt_ack; -- 
+process(clk, rst)
+begin
+if rst = '1' then
+	data_IN <= data_SWITCH;
+elsif clk = '1' and clk'event then
+	if interrupcion_pulsador = '0' and UART_buffer_data_present = '1' then -- FUENTE_I = from_PC
+		data_IN <= data_UART;
+	else
+		data_IN <= data_SWITCH;
+	end if;
+end if;
+end process;
 
-interrupt_ack1 <= interrupt_ack when from_PC = '1' else '0';
-interrupt_ack2 <= interrupt_ack when from_PC = '0' else '0';
-			
--- debo comunicar a los 2 dispositivos de entrada que estoy 
--- tomando datos 'UART_read_buffer'!
-			  
+interrupt_UART <= interrupt_ack when UART_buffer_data_present = '1' else '0';
+interrupt_PULSADOR <= interrupt_ack;
+
 end Behavioral;
-
